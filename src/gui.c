8c2bfe5dbd6164f44766698408323d7db16e6457
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-19 15:20:33 -0500

    Made a main functions within gui.c

--- src/gui.c
+++ src/gui.c
@@ -1,59 +1,21 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
 
-int
-main (int   argc,
-      char *argv[])
-{
+int main(int argc, char **argv) {
   GtkBuilder *builder;
-  GObject *window;
-  GObject *stack;
-  //  GObject *button;
-  GError *error = NULL;
+  GtkWidget *window;
 
-  gtk_init (&argc, &argv);
+  gtk_init(&argc, &argv);
 
-  /* Construct a GtkBuilder instance and load our UI description */
-  builder = gtk_builder_new ();
-  if (gtk_builder_add_from_file (builder, "builder.ui", &error) == 0)
-    {
-      printf("probelms!!!!!!!!!!!!!\n");
-      g_printerr ("Error loading file: %s\n", error->message);
-      g_clear_error (&error);
-      return 1;
-    }
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "builder.ui", NULL);
 
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+  gtk_builder_connect_signals(builder, NULL);
 
-  /* Connect signal handlers to the constructed widgets. */
+  g_object_unref(builder);
 
-  window = gtk_builder_get_object (builder, "window");
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-
-  printf("1\n");
-
-  stack = gtk_builder_get_object(builder, "tab1");
-  printf("2\n");
-  stack = gtk_builder_get_object(builder, "tab2");
-  printf("3\n");
-  stack = gtk_builder_get_object(builder, "tab3");
-  printf("4\n");
-  stack = gtk_builder_get_object(builder, "tab4");
-  printf("5\n");
-
-//  button = gtk_builder_get_object (builder, "button1");
-//  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-//  button = gtk_builder_get_object (builder, "button2");
-//  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-//  button = gtk_builder_get_object (builder, "quit");
-//  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
-
-  if (stack) {
-    printf("stack");
-  }
-
-  gtk_main ();
+  gtk_widget_show(window);
+  gtk_main();
 
   return 0;
 }
