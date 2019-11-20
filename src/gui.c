88ba594dd1eed91a9dce09031e052c2a97f08bfe
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-20 17:04:29 -0500

    Only one process view can be on at a time now

--- src/gui.c
+++ src/gui.c
@@ -13,12 +13,42 @@ void link_edit_buttons();
 void link_view_buttons();
 void link_help_buttons();
 
+GtkCheckMenuItem *my_proc;
+GtkCheckMenuItem *all_proc;
+GtkCheckMenuItem *active_proc;
 
 
 void quit_app(GtkWidget *widget, gpointer data) {
   mylog("Quitting Application");
   gtk_main_quit();
   exit(1);
+}
+
+void toggle_process_view(GtkWidget *widget, gpointer data) {
+  GtkCheckMenuItem *toggle = data;
+
+  /* Only change the other checks if toggle has been turned on */
+
+  if (gtk_check_menu_item_get_active(toggle)) {
+
+    /* Only turn the toggles off if they are on and are not the
+     * intended toggle */
+
+    if ((my_proc != toggle) &&
+        (gtk_check_menu_item_get_active(my_proc)) ) {
+      gtk_check_menu_item_set_active(my_proc, FALSE);
+    }
+
+    if ((all_proc != toggle) &&
+        (gtk_check_menu_item_get_active(all_proc)) ) {
+      gtk_check_menu_item_set_active(all_proc, FALSE);
+    }
+
+    if ((active_proc != toggle) &&
+        (gtk_check_menu_item_get_active(active_proc)) ) {
+      gtk_check_menu_item_set_active(active_proc, FALSE);
+    }
+  }
 }
 
 /*
@@ -29,6 +59,16 @@ void quit_app(GtkWidget *widget, gpointer data) {
 int init_task_manager(GtkBuilder *gui_builder) {
 	mylog("Initializing Task Manager");
   builder = gui_builder;
+
+  GObject *toggle = gtk_builder_get_object(builder, "view_my_processes");
+  my_proc = (GtkCheckMenuItem *) toggle;
+
+  toggle = gtk_builder_get_object(builder, "view_all_processes");
+  all_proc = (GtkCheckMenuItem *) toggle;
+
+  toggle = gtk_builder_get_object(builder, "view_active_processes");
+  active_proc = (GtkCheckMenuItem *) toggle;
+
   link_all_buttons();
   return 1;
 } /* init_task_manager() */
@@ -72,7 +112,13 @@ void link_edit_buttons() {
 }
 
 void link_view_buttons() {
-	return;
+  g_signal_connect(G_OBJECT(my_proc), "toggled", G_CALLBACK(toggle_process_view), my_proc);
+
+  g_signal_connect(G_OBJECT(all_proc), "toggled", G_CALLBACK(toggle_process_view), all_proc);
+
+  g_signal_connect(G_OBJECT(active_proc), "toggled", G_CALLBACK(toggle_process_view), active_proc);
+
+  return;
 }
 
 void link_help_buttons() {
