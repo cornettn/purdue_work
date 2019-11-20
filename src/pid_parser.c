26ae4c052dec99be1098a61e8c2fbaa13e8d466e
Daniel Nicholas Larken <dlarken@data.cs.purdue.edu>
2019-11-19 21:56:41 -0500

    Added minimum requirements comment in pid_parser.c

--- src/pid_parser.c
+++ src/pid_parser.c
@@ -14,10 +14,30 @@
 
 
 /*
- * Minimum requirements for pid_parser
- * 
- * 
- * 
+ * Minimum requirements for the Process Information tab
+ *   - Table with options to:
+ *       - Display list of all running processes
+ *       - Display processes only owned by current user
+ *       - Display the list in tree format
+ *   - This list should be updated at a fixed interval OR by using a refresh button
+ *   
+ *   - For each individual process you should have the ability to:
+ *       - Stop
+ *       - Continue
+ *       - Kill
+ *       - List memory maps
+ *       - List open files 
+ *   
+ *   - Each individual process should have a windo that shows:
+ *       - Process Name
+ *       - User
+ *       - State
+ *       - Memory
+ *       - Virtual memory
+ *       - Resident memory
+ *       - Shared memory
+ *       - CPU time
+ *       - Date/time started
  */
 
 
