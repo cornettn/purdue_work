c92336375148251cbd88aaf5124c1e1ccf6af1ac
cornettn <noahcornett@gmail.com>
2019-11-25 16:01:27 -0500

    Started implementing System Info

--- src/gui.c
+++ src/gui.c
@@ -4,19 +4,32 @@
 
 #include "logger.h"
 #include "button_linker.h"
+#include "sys_info.h"
+
+#define BUF_SIZE(1024)
+
+/* Function Declarations */
+
+int static link_all_buttons();
+void static link_menu_bar_buttons();
+void static link_monitor_buttons();
+void static link_edit_buttons();
+void static link_view_buttons();
+void static link_help_buttons();
+void static init_system_tab();
+
+/* Global Variables */
 
 GtkBuilder *builder;
-
-int link_all_buttons();
-void link_menu_bar_buttons();
-void link_monitor_buttons();
-void link_edit_buttons();
-void link_view_buttons();
-void link_help_buttons();
-
 GtkCheckMenuItem *my_proc;
 GtkCheckMenuItem *all_proc;
 GtkCheckMenuItem *active_proc;
+
+/*
+ * ##############################################
+ * START MENU BAR FUNCTIONS
+ * ##############################################
+ */
 
 /*
  * This function is used to quit to application.
@@ -24,7 +37,8 @@ GtkCheckMenuItem *active_proc;
 
 void quit_app(GtkWidget *widget, gpointer data) {
   mylog("Quitting Application");
-  stop_logging();
+  stop_logging();  
+  g_object_unref(builder);
   gtk_main_quit();
   exit(1);
 } /* quit_app() */
@@ -90,13 +104,14 @@ int init_task_manager(GtkBuilder *gui_builder) {
  * together as well as link the buttons to their appropiate function.
  */
 
-int link_all_buttons() {
+int static link_all_buttons() {
   mylog("Link All Buttons");
   mylog("Link Main Quit");
   GObject *main_window = gtk_builder_get_object(builder, "main_window");
   g_signal_connect(main_window, "destroy", G_CALLBACK(quit_app), NULL);
 
   link_menu_bar_buttons();
+  init_system_tab();
 
   return 1;
 } /* link_all_buttons() */
@@ -106,7 +121,7 @@ int link_all_buttons() {
  * to their respective action.
  */
 
-void link_menu_bar_buttons() {
+void static link_menu_bar_buttons() {
 	link_monitor_buttons();
 	link_edit_buttons();
 	link_view_buttons();
@@ -117,7 +132,7 @@ void link_menu_bar_buttons() {
  * This function is used to link all of the 'Monitor' menu bar buttons.
  */
 
-void link_monitor_buttons() {
+void static link_monitor_buttons() {
   GObject *quit = gtk_builder_get_object(builder, "monitor_quit_button");
 	g_signal_connect(quit, "activate", G_CALLBACK(quit_app), NULL);
 } /* link_monitor_buttons() */
@@ -126,7 +141,7 @@ void link_monitor_buttons() {
  * This function is used to link all of the 'Edit' menu bar buttons.
  */
 
-void link_edit_buttons() {
+void static link_edit_buttons() {
 	return;
 } /* link_edit_buttons() */
 
@@ -134,7 +149,7 @@ void link_edit_buttons() {
  * This function is used to link of the 'View' menu bar buttons.
  */
 
-void link_view_buttons() {
+void static link_view_buttons() {
   g_signal_connect(G_OBJECT(my_proc), "toggled", G_CALLBACK(toggle_process_view), my_proc);
   g_signal_connect(G_OBJECT(all_proc), "toggled", G_CALLBACK(toggle_process_view), all_proc);
   g_signal_connect(G_OBJECT(active_proc), "toggled", G_CALLBACK(toggle_process_view), active_proc);
@@ -144,8 +159,67 @@ void link_view_buttons() {
  * This function is used to link the 'Help' menu bar buttons.
  */
 
-void link_help_buttons() {
+void static link_help_buttons() {
 	return;
 } /* link_help_buttons() */
+
+
+/*
+ * ##############################################
+ * END MENU BAR FUNCTIONS
+ * ##############################################
+ */
+
+/*
+ * ##############################################
+ * START SYSTEM TAB FUNCTIONS
+ * ##############################################
+ */
+
+void static init_system_tab() {
+
+  /* Initialize sys_info functions */
+
+  if (init_sys_info() < 0) {
+    mylog("Error initializing sys_info");
+  }
+
+  /* Get system information using sys_info.c */
+
+  sys_info info = {0};
+  fill_info(&info);
+
+  /* Retrieve labels from builder */
+
+  GObject *os_type = gtk_builder_get_object(builder, "os_type_label");
+  GObject *os_verison = gtk_builder_get_object(builder, "os_release_version_label");
+  GObject *kernel_version = gtk_builder_get_object(builder, "kernel_version_label");
+  GObject *mem_amount = gtk_builder_get_object(builder, "memory_label");
+  GObject *proc_type = gtk_builder_get_object(builder, "processor_type_label");
+  GObject *disk_space = gtk_builder_get_object(builder, "disk_space_label");
+
+  /* Set labels to correct text */
+
+  gtk_label_set_text(os_type, info.OS_name);
+  gtk_label_set_text(os_verison, info.OS_version);
+  gtk_label_set_text(kernel_version, info.kernel_version);
+  gtk_label_set_text(mem_amount, info.mem_total);
+  gtk_label_set_text(proc_type, info.CPU_info);
+  
+  char disk_storage[1024] = {0}; 
+  sprintf(disk_storage, ".2f", info.disk_storage);
+  gtk_label_set_text(disk_space, disk_storage);
+
+}
+
+/*
+ * ##############################################
+ * END SYSTEM TAB FUNCTIONS
+ * ##############################################
+ */
+
+
+
+
 
 
